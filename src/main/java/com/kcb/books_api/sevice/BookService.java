073d1253d98881db0d0b4a84066bf6ce121c86ae
POLYCARP MOGAKA <kencont215408@kcbgroup.com>
2026-02-18 15:30:00 +0300

    Feat:Book Demo api

---
+++ src/main/java/com/kcb/books_api/sevice/BookService.java
@@ -0,0 +1,16 @@
+package com.kcb.books_api.sevice;
+
+import com.kcb.books_api.entity.Book;
+import com.kcb.books_api.model.BookDto;
+import jakarta.validation.Valid;
+import org.springframework.scheduling.config.Task;
+
+public interface BookService {
+    Book create(@Valid BookDto request);
+
+    Book getById(Long projectId);
+
+    Book update(Long bookId, @Valid BookDto request);
+
+    void delete(Long bookId);
+}
